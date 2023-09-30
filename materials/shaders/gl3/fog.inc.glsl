f457b53863c9590fbe5ef97b8ed80e3bcb549741
hogsy <markelswo@gmail.com>
2023-09-30 14:51:29 +0100

    Fixed fog.
    Signed-off-by: hogsy <markelswo@gmail.com>

--- materials/shaders/gl3/fog.inc.glsl
+++ materials/shaders/gl3/fog.inc.glsl
@@ -7,8 +7,8 @@ uniform float fogNear = 32.0;
 uniform vec4 fogColour = vec4(0.50, 0.83, 1.0, 0.1);
 
 vec4 CalculateFogTerm(vec4 n) {
-    float fogDistance = (gl_FragCoord.z / gl_FragCoord.w) / (fogFar * 100.0);
+    float fogDistance = (gl_FragCoord.z / gl_FragCoord.w) / (fogFar * 1.0);
     float fogAmount = 1.0 - fogDistance;
-    fogAmount *= -(fogNear / 100.0);
+    fogAmount *= -(fogNear / 1.0);
     return mix(n, fogColour, clamp(fogAmount, 0.0, 1.0));
 }
