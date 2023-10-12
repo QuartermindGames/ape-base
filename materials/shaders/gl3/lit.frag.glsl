a46d14b7f3f46847f920dd52f50051d3558fa3a5
hogsy <markelswo@gmail.com>
2023-10-12 18:22:29 +0100

    Updated shader types.
    Signed-off-by: hogsy <markelswo@gmail.com>

--- materials/shaders/gl3/lit.frag.glsl
+++ materials/shaders/gl3/lit.frag.glsl
@@ -1,25 +1,29 @@
-/**
- * Copyright (C) 2020-2021 Mark E Sowden <hogsy@oldtimes-software.com>
- */
+// Copyright © 2020-2023 OldTimes Software, Mark E. Sowden <hogsy@oldtimes-software.com>
+// Purpose: Base lighting fragment shader.
+// Author:  Mark E. Sowden
 
 #include "shared.inc.glsl"
 #include "lighting.inc.glsl"
 #include "fog.inc.glsl"
 
-void main() {
-	vec4 diffuse = texture(diffuseMap, vsShared.uv.st);
+void main()
+{
+    vec4 diffuse = texture(diffuseMap, vsShared.uv.st);
 
 #ifdef ALPHATEST
-	if (diffuse.a < 0.1)
-	{
-		discard;
-	}
+    if (diffuse.a < 0.1)
+        discard;
 #endif
 
-	vec3 n = normalize(texture(normalMap, vsShared.uv.st).rgb * 2.0 - 1.0);
-	n = normalize(vsShared.tbn * n);
+    vec3 n = normalize(texture(normalMap, vsShared.uv.st).rgb * 2.0 - 1.0);
+    n = normalize(vsShared.tbn * n);
 
-	vec4 lightTerm = CalculateLighting(n, normalize(vsShared.viewPos - vsShared.position));
-	vec4 outp = CalculateFogTerm(lightTerm * diffuse);
-	pl_frag = outp;
+#ifdef LIGHTING
+    vec4 lightTerm = CalculateLighting(n, normalize(vsShared.viewPos - vsShared.position));
+#else
+    vec4 lightTerm = sun.ambience;
+#endif
+    vec4 outp = CalculateFogTerm(lightTerm * diffuse);
+
+    pl_frag = outp;
 }
