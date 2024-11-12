7a33b497bd4c559a0d889cbbee3650a35721e648
Mark Sowden <hogsy@oldtimes-software.com>
2024-11-12 21:09:09 +0000

    Implement conditional specular lighting calculations
    Signed-off-by: Mark Sowden <hogsy@oldtimes-software.com>

--- materials/shaders/glsl/lighting.inc.glsl
+++ materials/shaders/glsl/lighting.inc.glsl
@@ -2,62 +2,78 @@
 
 #include "shared.inc.glsl"
 
-vec4 sterm( vec3 lDir, vec3 viewDir, vec3 specColour, float specPower, vec3 normal )
-{
-#ifdef CELL_SHADED
+//#define SUN
+#define SPECULAR
 
-	vec3 r = reflect( -lDir, normal );
-	float i = max( dot( viewDir, r ), 0.0 );
-	if ( i > 0.75 )
+#ifdef SPECULAR
+float sterm(vec3 lDir, vec3 viewDir, float specular, float specPower, vec3 normal)
+{
+	#ifdef CELL_SHADED
+
+	vec3 r = reflect(-lDir, normal);
+	float i = max(dot(viewDir, r), 0.0);
+	if (i > 0.75)
 	{
-		return vec4( specColour, 1.0 );
+		return specular;
 	}
 
-	return vec4( 0, 0, 0, 1 );
+	return 0.0f;
 
-#else
+	#else
 
-	vec3 halfway = normalize( lDir + viewDir );
-	return vec4( pow( max( dot( normal, halfway ), 0.0 ), specPower ) * specColour, 1.0 );
+	vec3 halfway = normalize(lDir + viewDir);
+	return pow(max(dot(normal, halfway), 0.0), specPower) * specular;
 
-#endif
+	#endif
 }
+#endif
 
-float lterm( vec3 n, vec3 l )
+float lterm(vec3 n, vec3 l)
 {
-#ifdef CELL_SHADED
+	#ifdef CELL_SHADED
 
-	float i = dot( n, l );
+	float i = dot(n, l);
 
 	float o = 0.0;
-	if ( i > 0.75 ) { o += ( i / 2 ); }
-	if ( i > 0.50 ) { o += ( i / 2 ); }
-	if ( i > 0.25 ) { o += ( i / 2 ); }
+	if (i > 0.75) { o += (i / 2); }
+	if (i > 0.50) { o += (i / 2); }
+	if (i > 0.25) { o += (i / 2); }
 
 	return o;
 
-#else
+	#else
 
-	return max( dot( n, l ), 0.0 );
+	return max(dot(n, l), 0.0);
 
-#endif
+	#endif
 }
 
-vec4 CalculateLighting( vec3 n, vec3 viewDir )
+vec4 CalculateLighting(vec3 n, vec3 viewDir)
 {
-	vec3 s = texture( specularMap, vsShared.uv.st ).rgb;
+	#ifdef SPECULAR
+	float specular = texture(specularMap, vsShared.uv.st).r;
+	#endif
 
-	// Apply the sun term first
-	vec3 lp = normalize( -sun.position );
+	vec3 lp;
 	vec4 o;
-	o += vec4( sun.colour.rgb, 1.0 ) * ( lterm( n, lp ) * sun.colour.a );
-	o += sterm( lp, viewDir, s * ( sun.colour.rgb * sun.colour.w ) * 2.0, 8.0, n );
 
-	lp = normalize( light.position - vsShared.position );
-	float d = length( light.position - vsShared.position );
-	float r = clamp( 1.0 - d * d / ( light.radius * light.radius ), 0.0, 1.0 );
-	o += ( lterm( n, lp ) * ( vec4( light.colour.rgb, 1.0 ) * light.colour.a ) ) * r;
-	o += ( sterm( lp, viewDir, s, 16.0, n ) * ( vec4( light.colour.rgb, 1.0 ) * light.colour.a ) ) * r;
+	#ifdef SUN
+	// Apply the sun term first
+	lp = normalize(-sun.position);
+	o += vec4(sun.colour.rgb, 1.0) * (lterm(n, lp) * sun.colour.a);
+	#ifdef SPECULAR
+	o += sterm(lp, viewDir, specular * (sun.colour.rgb * sun.colour.w) * 2.0, 8.0, n);
+	#endif
+	#endif
+
+	// "normal" light point term
+	lp = normalize(light.position - vsShared.position);
+	float d = length(light.position - vsShared.position);
+	float r = clamp(1.0 - d * d / (light.radius * light.radius), 0.0, 1.0);
+	o += (lterm(n, lp) * (vec4(light.colour.rgb, 1.0) * light.colour.a)) * r;
+	#ifdef SPECULAR
+	o += (sterm(lp, viewDir, specular, 16.0, n) * (vec4(light.colour.rgb, 1.0) * light.colour.a)) * r;
+	#endif
 
 	return sun.ambience + o;
 }
