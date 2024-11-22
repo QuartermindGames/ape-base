0c08c30f97b81f4b29eceded30ec06ab8d65289c
Mark Sowden <hogsy@oldtimes-software.com>
2024-11-22 17:40:23 +0000

    Selection shader for editor.
    Signed-off-by: Mark Sowden <hogsy@oldtimes-software.com>

---
+++ materials/shaders/glsl/editor/selection.frag.glsl
@@ -0,0 +1,13 @@
+/**
+ * Copyright © 2020-2024 Quartermind Games, Mark E. Sowden <hogsy@snortysoft.net>
+ */
+
+void main() {
+	vec2 square = fract(gl_FragCoord.xy / vec2(2.0, 2.0));
+	float gridFinal = step(square.x * square.y, 0.02);
+	if (gridFinal == 0) {
+		discard;
+	}
+
+	pl_frag = vec4(0.0, 0.0, 0.45 * gridFinal, 1.0);
+}
