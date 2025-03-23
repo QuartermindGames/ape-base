32ce70567a423664c3fb8bb585ea743b0d058ea6
Mark Sowden <hogsy@oldtimes-software.com>
2025-03-23 14:31:40 +0000

    Add an inout macro... urgh
    Signed-off-by: Mark Sowden <hogsy@oldtimes-software.com>

--- materials/shaders/glsl/shared.inc.glsl
+++ materials/shaders/glsl/shared.inc.glsl
@@ -7,6 +7,12 @@ const float PI = 3.14159265359;
 const float EPSILON = 0.0001;
 
 #if PLG_COMPILE_VERTEX == 1
+#define INOUT out
+#elif PLG_COMPILE_FRAGMENT == 1
+#define INOUT in
+#endif
+
+#if PLG_COMPILE_VERTEX == 1
 out
 #elif PLG_COMPILE_FRAGMENT == 1
 in
