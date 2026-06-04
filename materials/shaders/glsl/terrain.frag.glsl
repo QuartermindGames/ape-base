d8929624dec935890da524065c5333d7455abfa7
hogsy <markelswo@gmail.com>
2026-06-04 17:28:09 +0100

    Added tex def for gradient, so we can set clamp.
    Signed-off-by: hogsy <markelswo@gmail.com>

--- materials/shaders/glsl/terrain.frag.glsl
+++ materials/shaders/glsl/terrain.frag.glsl
@@ -9,19 +9,20 @@
 void main()
 {
 	vec2 uv = ( vsShared.position.xz ) / 128.0;
-	vec4 diffuse = PSX_GetDistanceTextureMip( diffuseMap, uv, vsShared.fadeFactor );
+	vec4 diffuse = texture( diffuseMap, uv ); //PSX_GetDistanceTextureMip( diffuseMap, uv, vsShared.fadeFactor );
 
-	vec4 detail = PSX_GetDistanceTextureMip( detailMap, uv / 8.0, vsShared.fadeFactor - 0.5 );
-	diffuse = mix(diffuse, detail, 0.25);
+	//vec4 detail = PSX_GetDistanceTextureMip( detailMap, uv / 8.0, vsShared.fadeFactor - 0.5 );
+	//diffuse = mix(diffuse, detail, 0.25);
 
 	vec3 n = normalize( texture( normalMap, vsShared.uv ).rgb * 2.0 - 1.0 );
 	n = normalize( vsShared.tbn * n );
 
 #ifdef LIGHTING
-	vec4 lightTerm = lighting_term( n, normalize( vsShared.viewPos - vsShared.position ) );
+	vec4 lightTerm = vsShared.colour;// + lighting_term( n, normalize( vsShared.viewPos - vsShared.position ) );
 #else
 	vec4 lightTerm = sun.ambience;
 #endif
+
 	vec4 outp = fog_apply( lightTerm * diffuse, vsShared.viewPos, vsShared.position );
 
     pl_frag = outp;
